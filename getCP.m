8dfce615b32b7737e7af64585985ea61fc703b1c
Dara Dimoff <dhd2788@rit.edu>
2021-01-18 13:22:51 -0500

    Added CP reading functionality via OCR

--- getCP.m
+++ getCP.m
@@ -1,3 +1,23 @@
 function cp = getCP(image)
+    % initialize to -1 for error checking
     cp = -1;
+    
+    % define a region of interest for where to check for CP
+    roi = [356 208 647-356 311-208];
+    
+    % perform OCR on the section of the image with the CP value
+    results = ocr(image, roi);
+    
+    cp = results.Text;
+    
+    % convert O's to zeros, as those often get misinterpreted
+    cp = strrep(cp,'O','0');
+    cp = strrep(cp,'o','0');
+    
+    % ignore all non-digit characters
+    cp = regexp(cp, '\d', 'match');
+    
+    % convert to string
+    cp = strjoin(string(cp),'');
+    
 end
