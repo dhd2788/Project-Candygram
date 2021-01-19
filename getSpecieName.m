032a7aae60a126b08e3458c0f1651c60a8ef1404
Dara Dimoff <dhd2788@rit.edu>
2021-01-19 17:00:30 -0500

    Implemented the ability to read the specie name from screenshot

--- getSpecieName.m
+++ getSpecieName.m
@@ -1,3 +1,25 @@
+%GETSPECIENAME Retrieves the name of a specie in a Pokemon Go screenshot of
+%the appraisal screen
+%   IMAGE = the image to read the data from. image must already be opened
 function specie = getSpecieName(image)
-    specie = 'Pidgey';
+
+    % define a region of interest for where to check for CP
+    roi = [272 963 866-272 1054-963];
+
+    % perform OCR on the section of the image with the CP value
+    results = ocr(image, roi);
+    
+    % strip all weird characters that are found
+    specie = regexp(results.Text,'\W*\w*','match');
+    specie = string(specie(1));
+    
+    % the case of some characters sometimes gets misinterpreted as capital
+    % handle that here
+    specie = lower(specie);
+    
+    % now capitalize the first character using regex
+    specie = convertStringsToChars(specie);
+    idx=regexp([' ' specie],'(?<=\s+)\S','start')-1;
+    specie(idx)=upper(specie(idx))
+    
 end
